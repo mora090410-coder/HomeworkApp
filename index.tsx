966f29cce6536928441a8887cc9d6ce05b97a822
mora090410-coder <mora090410@gmail.com>
2026-02-17 14:55:00 -0600

    Add new brand assets and update project configuration

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/src/context/ThemeContext';
