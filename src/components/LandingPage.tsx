7c5efea5f2ee99d08edf0c1c9e8abdcad812bff9
mora090410-coder <mora090410@gmail.com>
2026-02-19 18:24:07 -0600

    style(ui): increase landing page hero icon size to xl

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -26,7 +26,7 @@ export default function LandingPage() {
                     <img
                         src="/images/homework-icon-transparent.png"
                         alt="HomeWork"
-                        className="w-48 md:w-64 h-auto relative z-10"
+                        className="w-72 md:w-96 h-auto relative z-10"
                     />
                 </div>
 
